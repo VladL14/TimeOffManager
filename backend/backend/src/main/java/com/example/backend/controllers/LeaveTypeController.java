634316f2fb8367c68ecd95ff6202b0bad511dc0c
alex.cirlugea <alexcirlugea2@gmail.com>
2025-05-12 19:43:06 +0300

    feat: implement endpoint for track balance

--- backend/backend/src/main/java/com/example/backend/controllers/LeaveTypeController.java
+++ backend/backend/src/main/java/com/example/backend/controllers/LeaveTypeController.java
@@ -2,6 +2,9 @@ package com.example.backend.controllers;
 
 import com.example.backend.entities.LeaveType;
 import com.example.backend.repositories.LeaveTypeRepository;
+import com.example.backend.services.LeaveTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -10,13 +13,21 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
 public class LeaveTypeController {
     private final LeaveTypeRepository leaveTypeRepository;
+    private final LeaveTypeService leaveTypeService;
 
-    public LeaveTypeController(LeaveTypeRepository leaveTypeRepository) {
+    public LeaveTypeController(LeaveTypeRepository leaveTypeRepository, LeaveTypeService leaveTypeService) {
         this.leaveTypeRepository = leaveTypeRepository;
+        this.leaveTypeService = leaveTypeService;
     }
 
     @GetMapping
     public List<LeaveType> getAllLeaveTypes() {
         return leaveTypeRepository.findAll();
+    }
+
+    @GetMapping("/user/{userId}/vacation")
+    public ResponseEntity<Integer> getVacationBalance(@PathVariable int userId) {
+        int balance = leaveTypeService.getVacationBalance(userId);
+        return ResponseEntity.ok(balance);
     }
 }
